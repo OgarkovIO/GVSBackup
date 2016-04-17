e1597ba7c2e890a49da18bd00f3a21394774ba74
OgarkovIO <OgarkovIO@yandex.ru>
2016-04-17 20:26:05 +0300

    Update Form_Option.cs

--- GVSBackup/Form_Option.cs
+++ GVSBackup/Form_Option.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace GVSBackup//Закомментировать.
+namespace GVSBackup
 {
     public partial class Form_Option : Form
     {
@@ -29,38 +29,38 @@ namespace GVSBackup//Закомментировать.
 
         private void buttonSDP_Click(object sender, EventArgs e)
         {
-            //string listAddres = "";//Пустая, незадействованная сейчас переменная, на случай, если хранить настройки буду не в самой форме.
-            FolderBrowserDialog fdl = new FolderBrowserDialog();//Обявляем новый (new) экземпляр системного класса OpenFileDialog - fdl
-            if (fdl.ShowDialog() == System.Windows.Forms.DialogResult.OK)//Если (if) метод (.ShowDialog()) экземпляра (fdl) ==(оператор сравнени на равенство) System.Windows.Forms.DialogResult.OK - возвращаемое значение диалогового окна - ок .....
+            //string listAddres = "";
+            FolderBrowserDialog fdl = new FolderBrowserDialog();
+            if (fdl.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                textBoxSDP.Text = fdl.SelectedPath; //.....то, в PathTextBox(объект).Text(свойство получает или задаёт текущий текст в данном элементе) = (записываем) fdl(объект).FileName(свойство возвращает или задаёт имя файла выбранное в диалоговом окне)
+                textBoxSDP.Text = fdl.SelectedPath; 
             }
         }
 
         private void buttonORA_Click(object sender, EventArgs e)
         {
-            //string listAddres = "";//Пустая, незадействованная сейчас переменная, на случай, если хранить настройки буду не в самой форме.
-            FolderBrowserDialog fdl = new FolderBrowserDialog();//Обявляем новый (new) экземпляр системного класса OpenFileDialog - fdl
-            if (fdl.ShowDialog() == System.Windows.Forms.DialogResult.OK)//Если (if) метод (.ShowDialog()) экземпляра (fdl) ==(оператор сравнени на равенство) System.Windows.Forms.DialogResult.OK - возвращаемое значение диалогового окна - ок .....
+            //string listAddres = "";
+            FolderBrowserDialog fdl = new FolderBrowserDialog();
+            if (fdl.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                textBoxORA.Text = fdl.SelectedPath; //.....то, в PathTextBox(объект).Text(свойство получает или задаёт текущий текст в данном элементе) = (записываем) fdl(объект).FileName(свойство возвращает или задаёт имя файла выбранное в диалоговом окне)
+                textBoxORA.Text = fdl.SelectedPath; 
             }
 
         }
 
         private void buttonSUD_Click(object sender, EventArgs e)
         {
-            //string listAddres = "";//Пустая, незадействованная сейчас переменная, на случай, если хранить настройки буду не в самой форме.
-            FolderBrowserDialog fdl = new FolderBrowserDialog();//Обявляем новый (new) экземпляр системного класса OpenFileDialog - fdl
-            if (fdl.ShowDialog() == System.Windows.Forms.DialogResult.OK)//Если (if) метод (.ShowDialog()) экземпляра (fdl) ==(оператор сравнени на равенство) System.Windows.Forms.DialogResult.OK - возвращаемое значение диалогового окна - ок .....
+            //string listAddres = "";
+            FolderBrowserDialog fdl = new FolderBrowserDialog();
+            if (fdl.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                textBoxSUD.Text = fdl.SelectedPath; //.....то, в PathTextBox(объект).Text(свойство получает или задаёт текущий текст в данном элементе) = (записываем) fdl(объект).FileName(свойство возвращает или задаёт имя файла выбранное в диалоговом окне)
+                textBoxSUD.Text = fdl.SelectedPath; 
             }
         }
 
         private void Applay_Click(object sender, EventArgs e)
         {
-            Options_save ops = new Options_save();//Объявляем(посредством ключегого слова new) экземпляр созданного нами класса Options_save, ops.
+            Options_save ops = new Options_save();
             ops.FilePath = "config.xml";
             ops.LogPath = "log.txt";
             ops.OracleBackupPath = textBoxORA.Text;
@@ -69,8 +69,8 @@ namespace GVSBackup//Закомментировать.
             ops.CountOfFiles = NumberOfStoredFiles.Value.ToString();
             ops.CountOfDays = NumberOfDays.Value.ToString();
 
-            ops.saveConfig(ops.FilePath);//Применяем метод saveConfig() класса Options_save через экземпляр даннрого класса ops на текстовое поле(свойство .Text) нашего элемента PathTextBox.
-            Close();//Закрываем нашу форму, посредством метода Close();
+            ops.saveConfig(ops.FilePath);
+            Close();
         }
 
         private void Discard_Click(object sender, EventArgs e)
